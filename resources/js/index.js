ec80f81f15070918690ea0105d689db28ec2dadf
Aknell <71846760+Aknell@users.noreply.github.com>
2020-09-26 11:42:29 -0400

    Added tab functionality

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,43 +1,11 @@
-const $PageTabCollections = Array.from(document.querySelectorAll('.tabs'))
+$(document).ready(function() {
+  $('#tabs li').on('click', function() {
+    var tab = $(this).data('tab');
 
-if ($PageTabCollections.length > 0) {
-  const getActiveTabFromTabs = tabCollection => () => Array.from(tabCollection).find(tab => tab.classList.contains('is-active'))
-  const getActiveTabContentFromList = tabContentCollection => () => Array.from(tabContentCollection).find(content => content.classList.contains('is-active'))
-  $PageTabCollections.forEach(tabCollection => {
-    const hasUlTabs = tabCollection.children[0].nodeName === 'UL'
-    const tabContent = document.getElementById(tabCollection.dataset.contentTarget)
-    if (hasUlTabs) {
-      const tabs = Array.from(tabCollection.children[0].children)
-      const getActiveTab = getActiveTabFromTabs(tabs)
-      const getActiveContent = getActiveTabContentFromList(tabContent.children)
-      tabs.forEach(tabElm => {
-        tabElm.addEventListener('click', () => {
-          const activeTab = getActiveTab()
-          const activeContent = getActiveContent()
-          if (activeContent) {
-            activeContent.classList.toggle('is-active')
-          }
-          if (activeTab) {
-            activeTab.classList.toggle('is-active')
-          }
-          const contentElm = document.getElementById(tabElm.dataset.content)
-          if (contentElm) {
-            contentElm.classList.toggle('is-active')
-          } else {
-            console.error('Tab Content not found.')
-          }
-          tabElm.classList.toggle('is-active')
-        })
-      })
-      const activeTab = getActiveTab()
-      const activeContent = document.getElementById(activeTab.dataset.content)
-      if (activeContent) {
-        if (activeContent.classList.contains('is-active') === false) {
-          activeContent.classList.toggle('is-active')
-        }
-      }
-    } else {
-      console.error('Error: Tabs found that do not follow Bulma spec')
-    }
+    $('#tabs li').removeClass('is-active');
+    $(this).addClass('is-active');
+
+    $('#tabs p').removeClass('is-active');
+    $('p[data-content="' + tab + '"]').addClass('is-active');
   })
-}
+});
